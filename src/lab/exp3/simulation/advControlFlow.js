0a195789f6dda70d5570c4402345c726eda9e057
pratik432 <pratiksingh432@gmail.com>
2020-07-09 22:42:45 +0530

    buttonclick:CSE_3 issue solved

--- src/lab/exp3/simulation/advControlFlow.js
+++ src/lab/exp3/simulation/advControlFlow.js
@@ -167,17 +167,21 @@ window.view = {
     	this.clearDivs()
     	var selected_loop = this.getSelectedLoop()
 		var inputValue = document.getElementById('simpleLoopInput').value
-		if (selected_loop === 'for' && inputValue !== '' && !isNaN(model.inp) )
+		if (selected_loop === 'for' && inputValue !== '' && inputValue < 21 && !isNaN(model.inp) )
 		{
 			this.displayLoop('forLoopContent', 'codeContentFor1')
 		}
-		if (selected_loop === 'while' && inputValue !== '' && !isNaN(model.inp))
+		if (selected_loop === 'while' && inputValue !== '' && inputValue < 21 && !isNaN(model.inp))
 		{
 			this.displayLoop('whileLoopContent', 'codeContentWhile1')
 		}
-		if (selected_loop === 'do-while' && inputValue !== '' && !isNaN(model.inp))
+		if (selected_loop === 'do-while' && inputValue !== ''&& inputValue < 21 && !isNaN(model.inp))
 		{
 		 	this.displayLoop('dowhileLoopContent', 'codeContentDoWhile1')
+		}
+		if(inputValue > 20)
+		{    
+			alert("INVALID INPUT");
 		}
 		this.disableButton('btnStart')
 		this.changeClass( 'btnStart', 'buttonDisable startButton')
